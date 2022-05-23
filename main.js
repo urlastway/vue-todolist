46eadde13b427d850d1a20176ace3de1de125648
urlastway <andriy.semenchenko13@gmail.com>
2022-05-23 20:55:40 +0200

    mensione2

---
+++ main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+        todos: [
+            'andare in piscina',
+            'comprare la macchina',
+            'cucinare da mangiare',
+            'fare la doccia',
+            'andare a dormire',
+        ],
+        addTodo: "",
+    },
+    methods: {
+        pushTodo(){
+            if(this.addTodo !== ""){
+                this.todos.push(this.addTodo);
+                this.addTodo = "";
+            }
+        },
+        
+    },
+});
